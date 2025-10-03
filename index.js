75b1d98a8f59be42b39da5a44093d829b5abb48c
ShayerB-cuc <sbermude5@cuc.edu.co>
2025-10-03 10:25:12 -0500

    se hizo el primer ejercicio de 3 y se agregó un separador

---
+++ index.js
@@ -0,0 +1,8 @@
+let cantidad = 5
+
+for (let i = 1; i <= cantidad; i++) {
+    let caracter = "*"
+    let result = caracter.repeat(i);
+    console.log(result)
+}
+console.log("--------------------------")
