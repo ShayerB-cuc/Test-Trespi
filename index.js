200e0fa020515379596e51c52cc1b5f2e7d9c547
AmbruseCoding <juanestebancarvallocabrera@gmail.com>
2025-10-03 10:48:24 -0500

    Se creo el segundo ejercicio, triangulo invertido

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-let cantidad = 5
+const cantidad = 5;
 
 for (let i = 1; i <= cantidad; i++) {
-    let caracter = "*"
-    let result = caracter.repeat(i);
-    console.log(result)
+  let caracter = "*";
+  let result = caracter.repeat(i);
+  console.log(result);
 }
-console.log("--------------------------")
+console.log("--------------------------");
+
+for (let i = cantidad; i >= 1; i--) {
+  let texto = "";
+  for (let j = 1; j <= i; j++) {
+    texto = texto + "*";
+  }
+  console.log(texto);
+}
